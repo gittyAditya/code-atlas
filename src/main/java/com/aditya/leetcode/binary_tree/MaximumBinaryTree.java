344309e20cedf40497b1d3debe6021239707ca3f
Aditya Gautam <19ucs070@lnmiit.ac.in>
2023-04-08 05:57:22 +0530

    Add Binary Tree Solution

---
+++ src/main/java/com/aditya/leetcode/binary_tree/MaximumBinaryTree.java
@@ -0,0 +1,31 @@
+package com.aditya.leetcode.binary_tree;
+
+public class MaximumBinaryTree {
+
+        public TreeNode buildSubtree(int start, int end, int[] nums){
+
+            // Base Case if we remove condition (1) and (2)
+//            if(end < start)
+//                return null;
+            int max = -1;
+            int maxIndex = -1;
+            for(int i=start; i<=end; ++i){
+                if(nums[i] > max){
+                    max = nums[i];
+                    maxIndex = i;
+                }
+            }
+            TreeNode root = new TreeNode(max);
+            if(start != maxIndex)       // condition (1)
+                root.left = buildSubtree(start, maxIndex-1, nums);
+            if(end != maxIndex)         // condition (2)
+                root.right = buildSubtree(maxIndex+1, end, nums);
+            return root;
+        }
+
+        public TreeNode constructMaximumBinaryTree(int[] nums) {
+            int size = nums.length;
+            TreeNode root = buildSubtree(0, size-1, nums);
+            return root;
+        }
+}
